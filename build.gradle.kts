3453cddc2e8964593cd8d1b05ba7c34b987de28d
brevis <brevis.ua@gmail.com>
2022-07-14 12:27:06 +0300

    update build rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+val prop = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+}
+
 plugins {
     kotlin("jvm") version "1.6.21"
+    `java-library`
     `maven-publish`
 }
 
@@ -11,8 +20,28 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
     testImplementation(kotlin("test"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    disableAutoTargetJvm()
+}
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+            "Implementation-Version" to project.version))
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
 
 publishing {
@@ -32,8 +61,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/hexcheckers/engine")
             credentials {
-                username = project.findProperty("gpr.user").toString()
-                password = project.findProperty("gpr.key").toString()
+                username = prop.getProperty("gpr.user")
+                password = prop.getProperty("gpr.key")
             }
         }
     }
